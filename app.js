b6c9511632117ce84519a005cf727008122432c3
Zendrians <46094308+Zendrians@users.noreply.github.com>
2019-04-06 12:00:01 -0500

    heroku prepare

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express             = require("express");
 var app                 = express ();
-var port                = 3000
+var port                = process.env.PORT || 3000
 
-app.use(express.static('public'))
+app.use (express.static(__dirname + ("/public")));
 
 
 app.get('/', (req, res) => res.render('index.html'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.listen(port, () => console.log(`Animal Drinking listening on port ${port}!`));
+// app.listen(process.env.PORT, process.env.IP, function (){
+//     console.log ("Animal Drinking 'On'");
+// });
